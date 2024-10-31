692bee54406a6a3a154c303624e8213aa8dc9e6d
florian <florianlima51@gmail.com>
2024-10-31 14:18:48 +0100

    amélioration SCSS

--- Nat-m/header.php
+++ Nat-m/header.php
@@ -1,34 +1,35 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
+
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php wp_head(); ?>
-    
+
 </head>
 
 <body <?php body_class(); ?>>
     <?php wp_body_open(); ?>
-    
+
     <header class="site-header">
 
         <div class="container">
 
-            
+
             <div class="site-logo">
                 <a href="<?php echo esc_url(home_url('/')); ?>">
                     <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/logo.png" alt="Logo">
                 </a>
             </div>
 
-            
+
             <nav class="site-navigation">
 
-                
-              
+
+
                 <ul class="menu">
                     <?php
-                  
+
                     wp_nav_menu(array(
                         'theme_location' => 'primary',
                         'menu_class' => 'main-menu',
@@ -39,4 +40,3 @@
 
         </div>
     </header>
-    
